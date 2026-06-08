85927bd32ef07d26b6a9a6d4fff1c1f74185ea2c
aloispichler <45781387+aloispichler@users.noreply.github.com>
2026-06-08 12:26:13 +0200

    Add files via upload

--- LevenbergMarquardt.jl
+++ LevenbergMarquardt.jl
@@ -20,15 +20,15 @@ end
 function LevenbergMarquardt(fun::Function, funD::Function, x0::Vector{Float64}; maxEval= 1000, εAccuracy= 1e-7)
 	evalCount= 0
 	improvementFound= true; direction= Vector{Float64}(undef, length(x0))
-	grad= Vector{Float64}(undef, length(x0))
 	xMin= copy(x0); fMin= fun(x0); nfMin= norm(fMin); λ::Float64= 0.0
+	grad= Vector{Float64}(undef, length(x0))
 	Df2= Array{Float64}(undef, length(x0), length(x0))
 	while (improvementFound || nfMin > εAccuracy) && nfMin > 0.0 && evalCount < maxEval	# run until no improvement found
 		# @show xMin
 		# @show fMin
 		if improvementFound
 			Df= funD(xMin); Df2= Df'* Df
-			grad= Df'* fMin	# gradient of ½‖f(xMin)‖², steepest ascent
+			grad= Df'* fMin	# gradient of ½‖f(xMin)‖², the (-)direction of Gradient Descent
 		end
 		λ0= 1e-10+ 1e-10* maximum(diag(Df2))
 		if λ < λ0; λ= λ0; end 	# set initial regularization
@@ -38,19 +38,19 @@ function LevenbergMarquardt(fun::Function, funD::Function, x0::Vector{Float64}; 
 			direction= randn(length(x0))* (1e-7 + λ)
 			xTrial= xMin - direction
 		end
-		tmpDen= grad'* direction - direction'* Df2* direction/ 2
+		ReductionPredicted= grad'* direction - direction'* Df2* direction/ 2  # ignore the term f″(x)f(x), because f(x)≈ 0.
 		fTrial= fun(xTrial); nfTrial= norm(fTrial); evalCount+= 1
-		if nfTrial < nfMin # && tmpDen > 0
+		if nfTrial < nfMin # && ReductionPredicted > 0
 			xMin= xTrial; improvementFound= true
-			fMin= fTrial; tmpNom= (nfMin^2 - nfTrial^2)/ 2; nfMin= nfTrial
-			if tmpDen > 0
-				if tmpNom > 0.75* tmpDen	# gain_ratio ρ = tmpNom / tmpDen
+			fMin= fTrial; ReductionActual= (nfMin^2 - nfTrial^2)/ 2; nfMin= nfTrial
+			if ReductionPredicted > 0
+				if ReductionActual > 0.75* ReductionPredicted	# gain_ratio ρ = ReductionActual / ReductionPredicted
 					λ/= 3
-				elseif tmpNom < 0.25* tmpDen
+				elseif ReductionActual < 0.25* ReductionPredicted
 					λ*= 2
 				end
 			end
-		else # no improvement found. Try gradient descent
+		else # no improvement found. Try Gradient Descent, if iteration fails
 			λ*= 4; improvementFound= false
 		end
 		# @show evalCount, λ, nfMin #, xMin
